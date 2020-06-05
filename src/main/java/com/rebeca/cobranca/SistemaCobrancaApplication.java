9a8e340b2fbfc64daae283cc0d608d384947da4c
Rebeca Cruz <rebecacruz@me.com>
2020-06-04 21:21:51 -0300

    Inicio do  projeto cobranca, classe TituloController e pagina Cadastro Titulo

---
+++ src/main/java/com/rebeca/cobranca/SistemaCobrancaApplication.java
@@ -0,0 +1,13 @@
+package com.rebeca.cobranca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaCobrancaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaCobrancaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rebeca/cobranca/SistemaCobrancaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rebeca.cobranca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaCobrancaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
